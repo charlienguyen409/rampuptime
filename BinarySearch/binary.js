b6975f306a990bc993fd284d0a3d27f82b4c9397
tinkeringcharlie <tinkeringcharlie@gmail.com>
2018-07-10 21:05:50 -0700

    Fixed Binary Search
    Fixed Binary Search because it wasn't finding last item in array

--- BinarySearch/binary.js
+++ BinarySearch/binary.js
@@ -1,17 +1,30 @@
-//testing a commit through vsc integrated terminal
-await function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 5000);
-    });
+//Implement a binary search algorithm 
+var array = [1,2,3,4,5,6,7,8,9,10,11];
+var target = 4;
+var low = 0;
+var high = array.length -1;
+var mid;
+var guess;
+
+function BinarySearch(){
+  mid = Math.floor((low+high)/2);
+  guess = array[mid];
+  if(low > high){
+    return -1;
   }
-  
-  async function asyncCall() {
-    console.log('calling');
-    var result = await resolveAfter2Seconds();
-    console.log(result);
-    // expected output: "resolved"
+  else if(guess == target){
+    return mid;
   }
-  
-  asyncCall();
+  else if(guess < target){
+    low = mid + 1;
+    return BinarySearch();
+  }
+  else if(guess > target){
+    high = mid - 1;
+    return BinarySearch();
+  }
+}
+
+var result = BinarySearch();
+console.log(result);
+
